2f9fc0d46b8e78f7841a170b1922ee6ad6de285e
neerrajpanchal004 <npvishwakarma.143@gmail.com>
2024-03-08 21:56:20 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Signup from './components/Signup'
+import Login from './components/Login'
+import Contextapi from './context/Contextapi'
+import Header from './components/Header'
+import Home from './components/Home'
+import Footer from './components/Footer'
+
+
+
+const page = () => {
+  return (
+    <>
+    <Header />
+    <Home/>
+    <Footer />
+    
+    </>
+    
+  )
+}
+
+export default page
