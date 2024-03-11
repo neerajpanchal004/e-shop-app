f987a8e2146f01c6320cf59db9693245458fed09
neerrajpanchal004 <npvishwakarma.143@gmail.com>
2024-03-11 21:43:20 +0530

    check deploy

--- src/app/components/Mobiles.jsx
+++ src/app/components/Mobiles.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect,useState } from 'react'
 import { UserContext } from '../context/Contextapi'
 import { getData } from './Constant'
 import ProductCard from './ProductCard'
@@ -9,24 +9,25 @@ import Loader from './Loader'
 
 const Mobiles = () => {
     const { mobile, setMobile,setRoute } = useContext(UserContext)
+    const [first, setfirst] = useState()
 
 
 
     useEffect(() => {
         let apipoint = "/api/getProducts?category=mobiles";
-        getData(apipoint, setMobile);
+        getData(apipoint, setFirst);
 
     }, [])
-    if (mobile === undefined) {
+    if (first === undefined) {
         return <Loader />
     }
-    console.log(mobile)
+    console.log(first)
     setRoute("mobiles")
     return (
         <>
             <div className='grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 border-2  gap-x-5 gap-y-16 px-10'>
 
-                {mobile.map((Mobile) => {
+                {first.map((Mobile) => {
                     // const { name, image, price, storage,} = Mobile;
                     return (
                         <ProductCard key={Mobile.id} {...Mobile} />
